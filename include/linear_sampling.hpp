f82d6a25af298f04afcb6c160641e75f8acb94cd
Alithium <ali.bdieh@ensta-paris.fr>
2026-02-10 11:34:42 +0100

    Calcul de la composante du champs diffracté sur un bord

--- include/linear_sampling.hpp
+++ include/linear_sampling.hpp
@@ -18,14 +18,41 @@ public:
     // -------------------------------------------------------------------------
     // Calcul de la composante du champs diffracté sur un bord 
     // -------------------------------------------------------------------------
-    static void compute_u_s(const MeshP2& mesh, int n_mode, int boundary_tag, double k0, double h, vector<complexe>& u_s, vector<complexe>& u_n) {
-        
+    static void compute_u_s(const MeshP2& mesh, int n_mode, int boundary_tag, double direction, double L,double k0, double h,vector<complexe>& u_s, vector<complexe>& u_n) {
 
+        vector<int> visited(mesh.ndof(), 0);
+        for (const auto& tri : mesh.triangles) {
+            for (int edge_i = 0; edge_i < 3; ++edge_i) {
+                if (tri.edge_ref[edge_i] == boundary_tag){
+                    int idx_A = edge_i;
+                    int idx_B = (edge_i + 1) % 3;
+                    int idx_M = edge_i + 3;
+
+                    // Indices globaux dans la matrice
+                    int nodes_global[3] = {
+                        tri.node_ids[idx_A],
+                        tri.node_ids[idx_B], 
+                        tri.node_ids[idx_M]
+                    };
+
+                    for (const auto& idx : nodes_global) {
+                        if (!visited[idx]) {
+                            visited[idx] = 1;
+                            double x = mesh.nodes[idx].x;
+                            double y = mesh.nodes[idx].y;
+                            complexe exposant = exp(direction * complexe(0.,1.) *Fem::compute_beta(k0, h, n_mode)*L); // exp(i*direction*dot((x,y), direction_vector))
+                            complexe phi_n = Fem::evaluate_c_1d(y, h, n_mode)*exp(exposant); // Calcul de phi_n à ce point
+                            u_s[idx] = u_n[idx] - phi_n; // Contribution au champ diffracté sur le bord
+                        }        
+                    }        
+                }   
+            }
+        }
     }
     // -------------------------------------------------------------------------
     // Projection du champ diffracté n sur le mode m
     // -------------------------------------------------------------------------
-
+    
     // -------------------------------------------------------------------------
     // Calcul de F
     // -------------------------------------------------------------------------
